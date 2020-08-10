7c6eb76ea04b971c63a1b94634c8d743fb5f08ff
Kathleen Watson <kathleenwatson@Kathleens-MBP.attlocal.net>
2020-08-10 13:24:29 -0500

    created sourceList array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
@@ -8,3 +9,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'orbit-report';
 }
+
+var sourceList: Satellite[] = [
+    new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
+    new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
+    new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
+    new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
+    new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
+  ]; 
+  
+console.log(sourceList);
+/*
+  let siriusXM = new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true);
+  let catScanner = new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true);
+  let weberGrill = new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false);
+  let gps938 = new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true);
+  let iss = new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true);
+*/
